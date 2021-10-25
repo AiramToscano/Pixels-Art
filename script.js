b27c76f62e94807a1736f65066e9036398b32ce9
Airam Toscano <airamlobato@gmail.com>
2021-10-25 16:53:13 -0400

    resolvido do 1 ate o 6

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // constantes universais
-
+const colorblack1 = document.querySelector(".black")
 
 
 // criando meus quadros , logica parecida com o exercicio de jogo da velha apresentada na monitoria.
 function framepixels (){
     const tagmain = document.querySelector("#pixel-board");
-    for (let i = 0; i < 25; i+= 1) {
+    for (let i = 0; i < 25; i += 1) {
       let createframe = document.createElement('div');
       tagmain.appendChild(createframe);
       createframe.className = 'pixel';
     }   
 }
 framepixels();
+
+window.onload = function(){
+        
+    colorblack1.classList.add('selected');
+    
+   
+}
