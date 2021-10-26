46db4e87082a6c5b5d797983bcedb50e76d416de
Airam Toscano <airamlobato@gmail.com>
2021-10-26 11:09:28 -0400

    resolvido do 1 ao 8

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const colorred1 = document.querySelector('.red');
 const colloralls = document.querySelectorAll('.color');
 
 
+
+
+
+
 // criando meus quadros , logica parecida com o exercicio de jogo da velha apresentada na monitoria.
 function framepixels (){
     const tagmain = document.querySelector("#pixel-board");
@@ -12,9 +16,8 @@ function framepixels (){
       let createframe = document.createElement('div');
       tagmain.appendChild(createframe);
       createframe.className = 'pixel';
-      
-
-    }   
+       
+    }
 }
 framepixels();
 
@@ -27,42 +30,59 @@ window.onload = function(){
 }
 
 // parte da logica de remover funçoes foi tirada do site https://stackoverflow.com/questions/22270664/how-to-remove-a-class-from-elements-in-pure-javascript
-function colorred(){
+colorred1.addEventListener('click',function() {
   for(let i = 0 ; i < colloralls.length ; i +=1 ) {
       colloralls[i].classList.remove('selected');
  }
       colorred1.classList.add('selected');   
-}
-colorred();
+});
 
-colorred1.addEventListener('click',colorred);
-
-function colorblack(){
+colorblack1.addEventListener('click',function(){
   for(let i = 0 ; i < colloralls.length ; i +=1 ) {
       colloralls[i].classList.remove('selected');
  }
       colorblack1.classList.add('selected');   
-}
-colorblack();
+});
 
-colorblack1.addEventListener('click',colorblack);
-
-function colorblue(){
+colorblue1.addEventListener('click',function(){
   for(let i = 0 ; i < colloralls.length ; i +=1 ) {
       colloralls[i].classList.remove('selected');
  }
-      colorblue1.classList.add('selected');   
-}
-colorblue();
+      colorblue1.classList.add('selected');  
+       
+ });
 
-colorblue1.addEventListener('click',colorblue);
-
-function colorgreen(){
+  colorgreen1.addEventListener('click',function(){
   for(let i = 0 ; i < colloralls.length ; i +=1 ) {
       colloralls[i].classList.remove('selected');
  }
-      colorgreen1.classList.add('selected');   
-}
-colorgreen();
+      colorgreen1.classList.add('selected');  
+  });
 
-colorgreen1.addEventListener('click',colorgreen);
+  
+function printcor(){
+  const pixelalls = document.querySelectorAll('.pixel');
+  
+  
+  for(let i = 0; i< pixelalls.length; i += 1) {
+    pixelalls[i].addEventListener('click',function(e){
+      const pegarselect = document.querySelector('.selected');
+      
+      e.target.style.backgroundColor = pegarselect.style.backgroundColor;
+     
+    });
+  }
+}
+printcor();
+
+function createbackgroundcolor(){
+
+let pegarcolor= document.querySelectorAll('.color');
+
+pegarcolor[0].style.backgroundColor ="black";
+pegarcolor[1].style.backgroundColor ="red";
+pegarcolor[2].style.backgroundColor ="blue";
+pegarcolor[3].style.backgroundColor ="green";
+}
+createbackgroundcolor();
+
