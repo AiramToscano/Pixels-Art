2e38ab6cbbd530c789672f8c924250c0eeeade9c
Airam Toscano <airamlobato@gmail.com>
2022-04-24 11:50:03 -0400

    subindo repo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const colorblack1 = document.querySelector('.black');
+const colorblack1 = document.querySelector('.black'); // primeiro elemento é retornado
 const colorblue1 = document.querySelector('.blue');
 const colorgreen1 = document.querySelector('.green');
 const colorred1 = document.querySelector('.red');
-const colloralls = document.querySelectorAll('.color');
+const colloralls = document.querySelectorAll('.color'); // retorna uma lista de elementos presentes.
 
 // criando meus quadros , logica parecida com o exercicio de jogo da velha apresentada na monitoria.
 function framepixels() {
@@ -15,16 +15,19 @@ function framepixels() {
 }
 framepixels();
 
-window.onload = function window() {
+window.onload = () => {
   colorblack1.classList.add('selected');
   colorblue1.classList.remove('selected');
   colorred1.classList.remove('selected');
   colorgreen1.classList.remove('selected');
 };
 
-// parte da logica de remover funçoes foi tirada do site https://stackoverflow.com/questions/22270664/how-to-remove-a-class-from-elements-in-pure-javascript
+// // parte da logica de remover funçoes foi tirada do site https://stackoverflow.com/questions/22270664/how-to-remove-a-class-from-elements-in-pure-javascript
 colorred1.addEventListener('click', () => {
+  // eventlistener funciona como o elemento que eu quero adicionar  escutador o evento,
+  // o evento que eu quero interceptar.
   for (let i = 0; i < colloralls.length; i += 1) {
+    // eu preciso que minha classe select seja unica, sem está em outros elementos.
     colloralls[i].classList.remove('selected');
   }
   colorred1.classList.add('selected');
@@ -52,11 +55,13 @@ colorgreen1.addEventListener('click', () => {
 });
 
 function printcolor() {
-  const pixelalls = document.querySelectorAll('.pixel');
+  const pixelalls = document.querySelectorAll('.pixel'); // retornar uma lista com a classe pixel
   for (let i = 0; i < pixelalls.length; i += 1) {
     pixelalls[i].addEventListener('click', (e) => {
-      const pegarselect = document.querySelector('.selected');
-      e.target.style.backgroundColor = pegarselect.style.backgroundColor;
+      // console.log(e.target)
+      const pegarselect = document.querySelector('.selected'); // preciso retornar minha classe select pra saber onde ta
+      e.target.style.backgroundColor = pegarselect.style.backgroundColor; // vou implementar a delegação de  eventos.
+      // event target armazena o elemento que recebeu
     });
   }
 }
